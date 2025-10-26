66e19b7d33c0d938667e9e42ad8f3129dadde6cc
Degonawake <diegoisgonzales@gmail.com>
2025-10-26 13:40:59 -0600

    cambios inicio oct 2025

---
+++ SETEC/Migrations/20251026164628_subida1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SETEC.Migrations
+{
+    /// <inheritdoc />
+    public partial class subida1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
